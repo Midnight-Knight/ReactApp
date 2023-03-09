f64b7394fd717e8727077c74d6dae14001b1afe7
Leonid Deev <ltupov2003@yandex.ru>
2023-03-09 16:48:19 +0300

    Create base logic invert for LogicNightAndDayTheme

--- src/components/LogicNightAndDayTheme/LogicNightAndDayTheme.js
+++ src/components/LogicNightAndDayTheme/LogicNightAndDayTheme.js
@@ -2,16 +2,7 @@ import React from "react";
 
 export class Theme{
     #status;
-    #Background = ["rgba(36, 9, 53, 1)","rgba(219,246,202,1)"];
-    #BackgroundA = ["rgba(36, 9, 53, 0.5)",""];
-    #Block = ["rgba(32,33,79,1)",""];
-    #BlockA = ["rgba(32,33,79, 0.5)",""];
-    #BlockActive = ["rgba(123,104,238,1)",""];
-    #BlockActiveA = ["rgba(123,104,238,0.5)",""];
-    #Black = "rgba(0,0,0,1)";
-    #BlackA = "rgba(0,0,0,0.5)";
-    #White = "rgba(255,255,255,1)";
-    #WhiteA = "rgba(255,255,255,0.5)";
+    #invert = ["0%", "100%"]
 
     constructor() {
         if (localStorage.key(0) === "true")
@@ -33,17 +24,15 @@ export class Theme{
             case 0:
                 this.#status = 1;
                 localStorage.setItem(1, "Day");
-                document.getElementById("ThirdSection").style.background = this.#White;
+
                 break;
             case 1:
                 this.#status = 0;
                 localStorage.setItem(1, "Night");
-                document.getElementById("ThirdSection").style.background = this.#Black;
+
                 break;
         }
-        document.getElementById("FirstSection").style.background = this.#Background[this.#status];
-        document.getElementById("SecondSection").style.background = this.#Background[this.#status];
-        console.log(this.#Background[this.#status]);
+        
     }
 
 
