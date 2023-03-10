c055ca092806e6709e4a3b8564cfc40257b29f06
Leonid Deev <ltupov2003@yandex.ru>
2023-03-10 22:05:34 +0300

    Cancel LogicNightAndDayTheme

--- src/components/HeaderUI/Header.jsx
+++ src/components/HeaderUI/Header.jsx
@@ -1,20 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import "./Header.css";
 import documents from "./../../Image/documents.png";
 import {Clock} from "../ClockUI/Clock";
-import {Theme} from "../LogicNightAndDayTheme/LogicNightAndDayTheme";
 
 export const Header = () => {
-    let [A] = useState(new Theme());
-    function Click()
-    {
-        A.Change();
-    }
 
     return (
         <header id="header">
             <img src={documents} />
-            <button onClick={Click}></button>
             <Clock/>
         </header>
     );
