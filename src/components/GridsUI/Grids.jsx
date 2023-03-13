7a1894b9fbe69af6a1a9666312b3be2ba9792f2b
Leonid Deev <ltupov2003@yandex.ru>
2023-03-13 12:03:43 +0300

    First Version GridEducation

--- src/components/GridsUI/Grids.jsx
+++ src/components/GridsUI/Grids.jsx
@@ -66,12 +66,78 @@ export const GridTechnologies = () => {
 };
 
 export const GridEducation = () => {
+    let education = "Бакалавриат";
+    let dateStart = new Date(2021,9-1,1);
+    let dateActual = new Date();
+
+    if ((dateActual.getFullYear() - dateStart.getFullYear()) > 4 || ((dateActual.getFullYear() - dateStart.getFullYear()) == 4 && dateActual.getMonth()+1 >= 7)) {}
+    else
+    {
+        if (dateActual.getMonth()+1 >= dateStart.getMonth()+1)
+        {
+            education += " (" + (dateActual.getFullYear()-dateStart.getFullYear()+1) + " курс)";
+        }
+        else
+        {
+            education += " (" + (dateActual.getFullYear()-dateStart.getFullYear()) + " курс)";
+        }
+    }
+
+
     return(
       <div id="GridEducation">
           <div id="DevPhoto"></div>
-          <div id="UniversityText" className="EduBlock"></div>
-          <div id="BlockText" className="EduBlock"></div>
-          <div id="PhotoUniversity"></div>
+          <div id="UniversityText" className="EduBlock">
+              <ul>
+                  <li>
+                      <h6>Высшее образование:</h6>
+                  </li>
+                  <li>
+                      <h6>Университет:</h6>
+                  </li>
+                  <li>
+                      <h6>Факультет:</h6>
+                  </li>
+                  <li>
+                      <h6>Специальность:</h6>
+                  </li>
+                  <li>
+                      <h6>Год окончания:</h6>
+                  </li>
+              </ul>
+              <ul>
+                  <li>
+                      <p>{education}</p>
+                  </li>
+                  <li>
+                      <p>РТУ МИРЭА</p>
+                  </li>
+                  <li>
+                      <p>Институт информационных технологий</p>
+                  </li>
+                  <li>
+                      <p>Разработка и дизайн компьютерных игр и мультимедийных приложений</p>
+                  </li>
+                  <li>
+                      <p>2025</p>
+                  </li>
+              </ul>
+          </div>
+          <div id="BlockText" className="EduBlock">
+              <p>
+                  В процессе обучения в области проектирования и разработки мобильных приложений, интернет-ресурсов и клиент-серверных систем, я получил теоретическую и практическую
+                  подготовку с углубленной специализацией в игровой и медиаиндустрии.
+              </p>
+              <p>
+                  Мой учебный процесс проходил в компьютерных классах, оснащенных современным
+                  аппаратно-программным обеспечением и мультимедийным оборудованием, а также в специализированных лабораториях, оборудованных системами захвата движения и лицевой мимики,
+                  расширенной реальности, программным и техническим обеспечением для разработки компьютерной графики высокого качества.
+              </p>
+              <p>
+                  В рамках моего обучения, я уделял особое внимание дизайну цифровых продуктов и геймдизайну, компьютерной анимации и мультимедийным технологиям. Я также изучил
+                  инструментальные средства для разработки функционала и дизайна компьютерных игр и мультимедийных приложений.
+              </p>
+          </div>
           <div id="StatisticsBlock" className="EduBlock"></div>
       </div>
     );
